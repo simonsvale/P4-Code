edd1c167a2ab1e86cb135ba7f7a971bd34d4e29b
simonsvale <s.risbjerg@stofanet.dk>
2024-04-08 11:14:31 +0200

    Feature
    Made it possible for the sweep to utilize the correct subcarrier spacing case for each GSCN.

--- code/GSCNSweep.m
+++ code/GSCNSweep.m
@@ -1,5 +1,5 @@
-function GSCNSweep (rx, captureDuration, ofdmInfo, GSCNInfoFile)  
-    fprintf("Performing GSCN sweep!");
+function GSCNSweep (rx, GSCNInfoFile)  
+    disp("Performing GSCN sweep!");
 
     % Supress warning about table.
     warning('off', 'MATLAB:table:ModifiedAndSavedVarnames');
@@ -7,17 +7,33 @@ function GSCNSweep (rx, captureDuration, ofdmInfo, GSCNInfoFile)
     % Read GSCCN info table.
     GSCNInfo = readtable(GSCNInfoFile,TextType='String');
     
-    % Create a new table
+    % Create a new table, containing GSCN start values 
+    % and their subcarrier spacing.
     GSCNStartRange = GSCNInfo.GSCN_B;
+    GSCNScs = GSCNInfo.CASE;
 
     % Create a table for displaying all GCSN frequencies in the sweep.
     FR1Wave = single(2);
+
+    GSCNLength = height(GSCNStartRange);
+    
+    % Set the amount of frames to capture.
+    framesPerCapture = 2;
+    captureDuration = seconds((framesPerCapture+1)*10e-3);
+
+    nrbSSB = 20; % Number of resource blocks in an SSB 
     
     % Loop through all GSCN values, start index is 1 in matlab.
-    for index = 1:height(GSCNStartRange)  
+    for index = 1:GSCNLength  
         % Set new center frequency based on the GSCN index.
-        rx.CenterFrequency = hSynchronizationRasterInfo.gscn2frequency(GSCNStartRange(index));
-        
+        rx.CenterFrequency = hSynchronizationRasterInfo.gscn2frequency( GSCNStartRange(index) );
+       
+        % Set subcarrier spacing case.
+        scsSSB = hSSBurstSubcarrierSpacing( 'CASE '+GSCNScs(index) );
+
+        % OFDM demodulation information
+        ofdmInfo = nrOFDMInfo(nrbSSB,scsSSB,'SampleRate',rx.SampleRate);
+
         % Capture waveform
         waveform = variableSampleCapture(rx, captureDuration);
 
@@ -25,9 +41,11 @@ function GSCNSweep (rx, captureDuration, ofdmInfo, GSCNInfoFile)
         FR1Wave = cat(1, FR1Wave, waveform(:,1));
     end
 
+
     % Display figure
     figure;
-    nfft = ofdmInfo.Nfft;
+    nfft = ofdmInfo.Nfft * GSCNLength/3;
+
     spectrogram(FR1Wave,ones(nfft,1),0,nfft,'centered',rx.SampleRate,'yaxis','MinThreshold',-130);
     title('FR1 GSCN Spectrogram');
     
