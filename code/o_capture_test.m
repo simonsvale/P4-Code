d7c686fd3952db9f142f86b14541b526332e0ca6
simonsvale <s.risbjerg@stofanet.dk>
2024-04-09 09:07:27 +0200

    Feature
    Finished the ARFCN to center frequency function.

--- code/o_capture_test.m
+++ code/o_capture_test.m
@@ -1,9 +1,16 @@
+
+
+hej = ARFCN2Frequency(630048);
+
+disp(hej);
+
+return
+
 % Setup the SDR
 rx = hSDRReceiver('B210'); % Set radio type.
-rx.SDRObj.SerialNum = '8000758';
+rx.SDRObj.SerialNum = '8000748';
 rx.ChannelMapping = 1; % The antenna number.
 
-%rx.CenterFrequency =  2.11585e9; %3.71e9 %2.11585e9
 rx.Gain = 76; % Max 76 dBm
 rx.SampleRate = 31e6; % max ~41 MHz, theoretically 61.44 MHz.
 
