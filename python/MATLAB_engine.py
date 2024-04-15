e280bd27ef86e9808bf7aff377ea96abaab23737
Cernott <112928357+Peasniped@users.noreply.github.com>
2024-04-15 10:43:19 +0200

    Remove test-functions from MATLAB_engine.py

--- python/MATLAB_engine.py
+++ python/MATLAB_engine.py
@@ -1,44 +1,30 @@
 class MATLAB_engine():
     """
     Call this class as a with-object to ensure the connection is closed correctly:
-    with MATLAB_engine() as matlab:
-        result = matlab.start_test_multiply_numbers(5, 2)
-        print(result)
-
-    Requirement: matlab.engine-pakke i Python
-        Installation på Windows:
-        > cd "[Matlab-path]\extern\engines\python"
-          fx C:\Program Files\MATLAB\R2023b\extern\engines\python
-        > py -m pip install .
+    >>> with MATLAB_engine() as matlab:
+    >>>    result = matlab.start_test_multiply_numbers(5, 2)
+    >>>    print(result)
     """
     
     from os import getcwd
     import matlab.engine
 
     def __init__(self) -> None:
-        script_folder = "\code"
+        script_folder = "\\MATLAB\\functions"
         self.cwd = self.getcwd() + script_folder
-        self.cwd.replace('\\', '\\\\')
+        self.cwd.replace("\\", "\\\\")
 
         self.eng = self.matlab.engine.start_matlab()
         self.eng.cd(self.cwd, nargout=0)
+        # nargout er Num ARGuments OUT - eller antallet af outputs man forventer
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
         self.eng.quit()
-
-    def start_test_multiply_numbers(self, number_1:int, number_2:int) -> int:
-        """Takes two integers as a parameter, passes the parameter to MATLAB, to multiply, and captures and returns the output"""
-        output = self.eng._test_multiply_numbers(number_1, number_2)
-        return output
-
-    def start_device_test_script(self):
-        self.eng._test_device_test(nargout=0)
-        # nargout er Num ARGuments OUT - eller antallet af outputs man forventer
-
-    def function_find_radio(self, serial_number:str, gain:int) -> object:
+        
+    def find_radio(self, serial_number:str, gain:int) -> object:
         """Takes in a radio serial number and gain and returns a matlab.object with the radio settings
         
         :param serial_number: The serial number of the radio you wish to connect to
@@ -53,23 +39,8 @@ class MATLAB_engine():
         output = self.eng.function_find_radio(serial_number, gain)
         return output
 
-class test():
-    from os import getcwd
-
-    def __init__(self) -> None:
-        script_folder = "\code"
-        self.cwd = self.getcwd() + script_folder
-        self.cwd.replace('\\', '\\\\')
-
-    def function_arfcn_sweep(self):
-        
-        with open(self.cwd + "\\ARFCNDanmark.csv") as file:
-            print(file.read())
-
 if __name__ == "__main__":
-    test().function_arfcn_sweep()
-    
-    #with MATLAB_engine() as matlab:
-        #result = matlab.start_test_multiply_numbers(5, 2)
+    with MATLAB_engine() as matlab:
+        result = matlab.start_test_multiply_numbers(5, 2)
         
         
