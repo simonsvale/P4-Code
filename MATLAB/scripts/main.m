ac0e025d47b006c1eab8ad25c583aaafa0a78691
TheBear <drachmann.bjorn@gmail.com>
2024-04-15 11:49:07 +0200

    Bugfix tx object
    Creating a transmit object from the same radio throws error?

--- MATLAB/scripts/main.m
+++ MATLAB/scripts/main.m
@@ -1,19 +1,19 @@
 % Setup the SDR
 rx = hSDRReceiver('B210'); % Set radio type.
-rx.SDRObj.SerialNum = '8000758';
+rx.SDRObj.SerialNum = '8000748';
 rx.ChannelMapping = 1; % The antenna number?
 
 rx.Gain = 76; % Max 76 dBm
 rx.SampleRate = 31e6; % max ~41 MHz, theoretically 61.44 MHz.
 
 % Setup the SDR also for tx
-tx = comm.SDRuTransmitter(Platform='B210');
-tx.SerialNum ='8000748';
-tx.CenterFrequency = 2450000000;
-tx.Gain = 8;
-tx.ChannelMapping = 1;
-tx.TransportDataType = 'int16';
-tx.EnableBurstMode = false;
+% tx = comm.SDRuTransmitter(Platform='B210');
+% tx.SerialNum ='8000748';
+% tx.CenterFrequency = 2450000000;
+% tx.Gain = 8;
+% tx.ChannelMapping = 1;
+% tx.TransportDataType = 'int16';
+% tx.EnableBurstMode = false;
 
 
 
