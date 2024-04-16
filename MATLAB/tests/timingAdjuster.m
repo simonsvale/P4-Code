8047bf31cb8a5d8db2baef4be6b866d7e1caf885
TheBear <drachmann.bjorn@gmail.com>
2024-04-16 12:20:18 +0200

    timingAdjuster final testing code done

--- MATLAB/tests/timingAdjuster.m
+++ MATLAB/tests/timingAdjuster.m
@@ -2,9 +2,25 @@
 % t.TasksToExecute = 3;
 % start(t);
 
+disp("Trying timer timings (precise)");
 t = timer;
 t.ExecutionMode = 'fixedRate';
 t.Period = 1;
 t.TimerFcn = @(~,~) disp(datetime('now','Format','HH:mm:ss.SSSSSS'));
-t.TasksToExecute = 10;
+t.TasksToExecute = 3;
 start(t);
+pause(5);
+
+disp("Trying pause timings (unprecise)");
+desired_frequency = 1; % Run code every 1 second
+period = 1 / desired_frequency;
+num_iterations = 5; % Run code 3 times
+for i = 1:num_iterations
+    start_time = datetime('now');
+    disp(datetime('now','Format','HH:mm:ss.SSSSSS'));
+    end_time = start_time + seconds(period);
+    while datetime('now') < end_time
+        pause(0.01); % small pause to avoid busy waiting
+    end
+end
+
