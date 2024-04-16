83f121b454e958f757d8ab5f5527a4b51b6e89b5
TheBear <drachmann.bjorn@gmail.com>
2024-04-16 10:54:13 +0200

    Alternative timingAdjuster using matlabs timer to schedule functions

--- MATLAB/tests/timingAdjuster.m
+++ MATLAB/tests/timingAdjuster.m
@@ -1,33 +1,23 @@
-
 timing = 0.02;
 duration = timing;
-
-for n = 1:1000
-    
-    % Wait duration
-    tic;
-    while toc < duration
+timer1 = tic;
+startTime = tic;
+for n = 1:100
+    tic;    % Record current time
+    while toc < duration % Wait 20ms(approx) into the future
     end
-
-    % Do code / transmission
-    tic;
-    
     for i = 1:4000
         fprintf("");
     end
-
-    disp("Nr: "+n+", dur: "+duration+", date: "+datestr(now,'SS.FFF'));
-
-    % Take new timestamp
-    duration = timing-(toc);
-
+    % Do code / transmission
+    tic;
+    disp("Nr: "+n+", dur: "+duration+", date: "+datestr(now,'SS.FFF') + " | toc=" + toc);
+    duration = timing-(toc);    % Adjust next cycle to wait less if transmission was time-consuming
 end
+% disp("Total elapsed time: ", toc(timer1));
 
-
-
-
-
-
-
-
-
+t = timer;
+t.TimerFcn = @(~,thisEvent)disp([thisEvent.Type ' executed ' datestr(thisEvent.Data.time,'dd-mmm-yyyy HH:MM:SS.FFF')]);
+t.TasksToExecute = 10;
+t.ExecutionMode = 'fixedRate';
+start(t)
