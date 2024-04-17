742e45ade180aefb6c36d4654c634c1c453dc389
simonsvale <s.risbjerg@stofanet.dk>
2024-04-17 09:21:53 +0200

    Added sine wave generation and transmission.
    Co-Authored-By: Mikkel-Coder <71432330+Mikkel-Coder@users.noreply.github.com>

---
+++ MATLAB/utils/scheduledTransmission.m
@@ -0,0 +1,65 @@
+function status = scheduledTransmission(centerFrequency, waveSampleRate, duration)
+    
+    % 2.11585e9, 40000, 0.01
+
+    % Generation
+    if (waveSampleRate < 9800)
+        status = 1;
+        return
+    end
+    
+
+    % Generate sine wave for transmission.
+    disp("Generating sine wave!");
+
+    sineWave = dsp.SineWave('Frequency', centerFrequency, ...
+        'Amplitude', 1, ...
+        'PhaseOffset', 1, ...
+        'SampleRate', waveSampleRate , ...
+        'ComplexOutput', 0, ...
+        'SamplesPerFrame', duration * waveSampleRate);
+    
+    waveform = sineWave();
+
+    tx = comm.SDRuTransmitter(Platform='B210');
+    tx.SerialNum = findsdru().SerialNum;
+    tx.CenterFrequency = 2.11585e9;
+    tx.Gain = 76;
+    tx.ChannelMapping = 1;
+    tx.LocalOscillatorOffset = 1;
+    tx.PPSSource = 'Internal';
+    tx.ClockSource = 'Internal';
+    %tx.MasterClockRate = masterClockRate;
+    tx.InterpolationFactor = 1;
+    tx.TransportDataType = 'int16';
+    tx.EnableBurstMode = false;
+    
+    % This is needed, to configure the radio.
+    pause(5);
+    disp("Configuring radio!")
+    tx(waveform);
+    
+    
+    periodicity = 0.02;
+    times = 5000;
+    
+    disp("transmission started!");
+    
+    for i = 1:times
+        tic;
+        while toc < 0.01
+        tx(waveform);
+        end
+    
+        tic;
+        while toc < periodicity
+        end
+    end
+    
+    disp("Transmission stopped!");
+    release(tx);
+
+
+end
+
+
