982d0a0501a2d66ec298ece81801e39b6c0de34c
Mikkel-Coder <ms22@student.aau.dk>
2024-04-17 14:30:27 +0200

    Updated displayWave for smartlab testing

--- MATLAB/utils/displayWave.m
+++ MATLAB/utils/displayWave.m
@@ -2,8 +2,8 @@
 rx = hSDRReceiver('B210'); % Set radio type.
 
 % Get serial number
-radio = findsdru();
-rx.SDRObj.SerialNum = radio(1).SerialNum;
+%radio = findsdru();
+%rx.SDRObj.SerialNum = radio(1).SerialNum;
 
 % Reset variable to avoid problems
 clear radio;
@@ -14,23 +14,22 @@ rx.Gain = 76; % Max 76 dBm
 rx.SampleRate = 31e6; % max ~41 MHz, theoretically 61.44 MHz.
 
 % Convert ARFCN to center frequency.
-rx.CenterFrequency = 2.19965e9;
-
+rx.CenterFrequency = 3.70992e9;
 
 % Set subcarrier spacing case from center frequency.
 scsOptions = hSynchronizationRasterInfo.getSCSOptions(rx.CenterFrequency);
 scs =  scsOptions(1);
 
 % Capture waveform
-waveform = variableSampleCapture(rx, milliseconds(2000));
+waveform = variableSampleCapture(rx, milliseconds(60));
 
 nrbSSB = 10; % Number of resource blocks in an SSB
 scsNumeric = double(extract(scs,digitsPattern));
 ofdmInfo = nrOFDMInfo(nrbSSB,scsNumeric);
 
-% Display spectrogram of received waveform
+% Display spectrogram of received waveform  
 figure;
-nfft = ofdmInfo.Nfft;
+nfft = ofdmInfo.Nfft * 32;
 spectrogram(waveform(:,1),ones(nfft,1),0,nfft,'centered',rx.SampleRate,'yaxis','MinThreshold',-130);
 title('Spectrogram of the Received Waveform')
 
