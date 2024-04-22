3e76ed7cae831e8f193ee21a6d6dfc0bf5225f24
simonsvale <s.risbjerg@stofanet.dk>
2024-04-22 14:41:30 +0200

    Added a function for a dumb jammer.
    Co-Authored-By: Andersen2407 <113008478+Andersen2407@users.noreply.github.com>

---
+++ MATLAB/functions/dumbSSBJam.m
@@ -0,0 +1,56 @@
+function dumbSSBJam(rx, tx, centerFrequency, duration)
+
+    % Check if any SSBs exists on the given center frequency.
+    [frequency, timestamp] = frequencySweep(rx, centerFrequency, 30);
+    
+    % Check if no SSBs were found.
+    if (isempty(frequency))
+        disp("No SSBs found on the given frequency.");
+        return
+    end
+
+    % Setup jamming signal.
+    sineWaves = dsp.SineWave('Frequency', centerFrequency, ...
+    'Amplitude', 1, ...
+    'PhaseOffset', 0, ...
+    'SampleRate', 10e3 , ...
+    'ComplexOutput', 0, ...
+    'SamplesPerFrame', 200e3);
+
+    % Generation
+    waveform = sineWaves();
+
+    waveform = resample(waveform, 1000, tx.MasterClockRate/500);
+
+    tx.ChannelMapping = 1;
+    tx.LocalOscillatorOffset = 1;
+    tx.PPSSource = 'Internal';
+    tx.ClockSource = 'Internal';
+    tx.InterpolationFactor = 1;
+    tx.TransportDataType = 'int16';
+    tx.EnableBurstMode = false;
+
+    waveform = repmat(waveform, 1, 1);
+    
+    % Configure transmission, is needed due to the FPGA.
+    disp("Configuring transmission!")
+    tx.CenterFrequency = centerFrequency;
+    
+    % Initial pause is needed for the SDR to configure correctly. 
+    pause(5);
+    tx(waveform);
+    pause(5);
+
+    disp("Starting transmission!");
+    
+    tic;
+    while toc < duration
+        tx(waveform);
+    end
+    
+
+    disp("Done Transmitting!");
+
+end
+
+
--- MATLAB/tests/COnfTest.m
+++ MATLAB/tests/COnfTest.m
@@ -2,7 +2,7 @@
 [rx, tx] = configureSDR('B210','');
 
 % Jam duration in milliseconds.
-smartSSBJam(rx, tx, 2.11585e9, 30);
+dumbSSBJam(rx, tx, 2.11585e9, 30);
 
 delete(rx);
 delete(tx);
