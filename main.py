9154f2b00c60da8b92ed930d014b04d4e1982e85
Mikkel-Coder <ms22@student.aau.dk>
2024-05-07 16:05:53 +0200

    removed testing from main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from python.source_code.cli import CLI
-from python.test.test_radio_controller import TestRadioController
 
 if __name__ == "__main__":
-    # CLI().run()
-    TestRadioController().test_frequency_sweep()
+    CLI().run()
