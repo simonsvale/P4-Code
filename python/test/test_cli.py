8b07c4bd810201c04f45872581347c4808f66b9c
Mikkel-Coder <ms22@student.aau.dk>
2024-05-08 14:18:52 +0200

    began unit testing for CLI python

--- python/test/test_cli.py
+++ python/test/test_cli.py
@@ -1,13 +1,73 @@
 import unittest
-
+import os
+import subprocess
 from source_code.cli import CLI
 
 
 class TestCLI(unittest.TestCase):
 
+    def setUp(self) -> None:
+        # Setup a CLI process for testing
+
+        # Change path to project dir to run the CLI process from main.py
+        project_dir: str = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
+        cmd = ["python3", "main.py"]
+        self.cli_process = subprocess.Popen(
+            cmd,
+            cwd=project_dir,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+    def tearDown(self) -> None:
+        # Close the file descriptors before terminate if needed
+        while not self.cli_process.stdin.closed:
+            self.cli_process.stdin.close()
+
+        while not self.cli_process.stdout.closed:
+            self.cli_process.stdout.close()
+
+        while not self.cli_process.stderr.closed:
+            self.cli_process.stderr.close()
+
+        # Terminate the cli process if it is alive
+        while self.cli_process.poll() is None:
+            self.cli_process.terminate()
+
+    def test_discover_radio(self):
+        """Test for invalid radio chose."""
+
+        # Select discover Radio option
+        self.cli_process.stdin.write(b"1\n")
+        self.cli_process.stdin.flush()
+
+        # Chose an invalid radio option
+        stdout, stderr = self.cli_process.communicate(b"invalid\n")
+
+        # Check if we got an invalid choice
+        output = stdout.decode()
+        self.assertIn("Invalid radio choice.", output)
+
     def test_frequency_sweep(self):
-        # Test for raise exception when no radio is configured
-        self.assertRaises(Exception, CLI()._frequency_sweep())
+        self.skipTest(reason="No yet implemented unit test")
+
+    def test_choose_attack(self):
+        self.skipTest(reason="No yet implemented unit test")
+
+    def test_run_attack(self):
+        self.skipTest(reason="No yet implemented unit test")
+
+    def test_reset_selected_attack(self):
+        self.skipTest(reason="No yet implemented unit test")
+
+    def test_sss_jamming(self):
+        self.skipTest(reason="No yet implemented unit test")
+
+    def test_pdch_exploit(self):
+        self.skipTest(reason="No yet implemented unit test")
+
 
 """
     def _frequency_sweep(self) -> None:
